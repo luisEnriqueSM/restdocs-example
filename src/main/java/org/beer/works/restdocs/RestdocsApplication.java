d6f5d07a8bbf32270c1d1340493c1a4ae3d3b4e1
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-07-29 10:45:10 -0600

    Initial commit

---
+++ src/main/java/org/beer/works/restdocs/RestdocsApplication.java
@@ -0,0 +1,13 @@
+package org.beer.works.restdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestdocsApplication.class, args);
+	}
+
+}
